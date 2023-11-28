0d727ea0cb084d75c7979611e64de5879139baf5
Barış Özgen <barisozgen@hotmail.com>
2023-11-29 00:40:16 +0300

    Header Exchange Done

--- RabbitMQBaris/RabbitmqHelloWorld.Publisher/HeaderBaris.cs
+++ RabbitMQBaris/RabbitmqHelloWorld.Publisher/HeaderBaris.cs
@@ -15,14 +15,14 @@ namespace RabbitmqHelloWorld.Publisher
             factory.Uri = new Uri("amqps://znxwrwvv:cTBKdtcXCNroP24le4FpPgl1PsDNhUC6@cow.rmq2.cloudamqp.com/znxwrwvv");
             using var connection = factory.CreateConnection();
             var channel = connection.CreateModel();
-            channel.ExchangeDeclare("logs-fanout", durable: true, type: ExchangeType.Fanout);
-            Enumerable.Range(1, 50).ToList().ForEach(x =>
-            {
-                string message = $"log {x}";
-                var messageBody = Encoding.UTF8.GetBytes(message);
-                channel.BasicPublish("logs-fanout","", null, messageBody);
-                Console.WriteLine($"Message is sent : {message}");
-            });
+            channel.ExchangeDeclare("header-exchange", durable: true, type: ExchangeType.Headers);
+            Dictionary<string, object> headers = new Dictionary<string, object>();
+            headers.Add("format", "pdf");
+            headers.Add("shape2", "a4");
+            var properties = channel.CreateBasicProperties();
+            properties.Headers = headers;
+            channel.BasicPublish("header-exchange", string.Empty, properties, Encoding.UTF8.GetBytes("my header message"));
+            Console.WriteLine("Message is sent");
             Console.ReadLine();
         }
     }
--- RabbitMQBaris/RabbitmqHelloWorld.Subscriber/HeaderBaris.cs
+++ RabbitMQBaris/RabbitmqHelloWorld.Subscriber/HeaderBaris.cs
@@ -6,24 +6,28 @@ namespace RabbitmqHelloWorld.Subscriber;
 
 public class HeaderBaris
 {
-static void Main(string[] args)
+        static void Main(string[] args)
         {
             var factory = new ConnectionFactory();
             factory.Uri = new Uri("amqps://uhshoatb:4tRfDsemduk6BCrsZaIvfQgOhLsMtf-t@fish.rmq.cloudamqp.com/uhshoatb");
             using var connection = factory.CreateConnection();
             var channel = connection.CreateModel();
-            var randomQueueName = channel.QueueDeclare().QueueName;
-            //when app is stop running our queue will be deleted by QueueBind
-            channel.QueueBind(randomQueueName, "logs-fanout", "", null);
+            channel.ExchangeDeclare("header-exchange", durable: true, type: ExchangeType.Headers);
             channel.BasicQos(0, 1, false);
             var consumer = new EventingBasicConsumer(channel);
-            channel.BasicConsume(randomQueueName,false, consumer);
-            Console.WriteLine("Logs are listening...");
+            var queueName = channel.QueueDeclare().QueueName;
+            Dictionary<string, object> headers = new Dictionary<string, object>();
+            headers.Add("format", "pdf");
+            headers.Add("shape", "a4");
+            headers.Add("x-match", "any");
+            channel.QueueBind(queueName, "header-exchange",String.Empty,headers);
+            channel.BasicConsume(queueName,false, consumer);
+            Console.WriteLine("Logs listening...");
             consumer.Received += (object sender, BasicDeliverEventArgs e) =>
             {
                 var message = Encoding.UTF8.GetString(e.Body.ToArray());
                 Thread.Sleep(1500);
-                Console.WriteLine("Received:" + message);
+                Console.WriteLine("Received Message:" + message);
                 channel.BasicAck(e.DeliveryTag, false);
             };
             Console.ReadLine();
